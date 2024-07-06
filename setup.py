138cf1346f737dc2732a695a71757d61fcc42a10
HendrikPlett <hendrik.plett@web.de>
2024-07-07 00:59:58 +0200

    Changed package name from src to golem

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='Golem',  
-    version='0.1',            
-    packages=['src'], 
+    version='0.2',            
+    packages=['golem'], 
     install_requires = requirements
 )
