fe0be8bd4a6aff08fd37e89f7bc4d28336d35beb
HendrikPlett <hendrik.plett@web.de>
2024-07-07 01:20:09 +0200

    Version 0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='Golem',  
-    version='0.3',            
+    version='0.4',            
     packages=['golem'], 
     install_requires = requirements
 )
