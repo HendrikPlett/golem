dc8aae55813ce284b4c144034753dbc67b470e56
HendrikPlett <hendrik.plett@web.de>
2024-07-07 02:16:32 +0200

    Fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='Golem',  
-    version='0.9',            
+    version='0.10',            
     packages=['golempckg', 
               'golempckg.models', 
               'golempckg.trainers', 
